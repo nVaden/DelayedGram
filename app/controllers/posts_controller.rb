e8f2cdc61bbb1349daeb08bacc56efd5a6dd9120
Nicholas Vaden <nickvaden11@Live.com>
2017-02-23 15:14:36 -0500

    Basic functionality complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+	before_action :set_post, only: [:show, :edit, :update, :destroy]
 	def index
 		@posts = Post.all
 	end
@@ -6,8 +7,11 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 	def create
-		@post = Post.create(post_params)
+		if @post = Post.create(post_params)
 		redirect_to posts_path
+		else
+			render :new
+		end
 	end
 	def show
 		@post = Post.find(params[:id])
@@ -22,10 +26,19 @@ class PostsController < ApplicationController
 		@post.update(post_params)
 		redirect_to(post_path(@post))
 	end
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to posts_path
+	end
 
 	private
 
 	def post_params  
   		params.require(:post).permit(:image, :caption)
-	end  
+	end
+
+	def set_post
+	@post = Post.find(params[:id])  
+	end
 end
